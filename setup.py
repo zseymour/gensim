02b539a3ad733aed70daea0e2416032fd60a398d
Zachary Seymour <zseymou1@binghamton.edu>
2015-09-29 13:16:41 -0400

    Added Visual word2vec to extension list.

--- setup.py
+++ setup.py
@@ -120,6 +120,9 @@ setup(
         Extension('gensim.models.doc2vec_inner',
             sources=['./gensim/models/doc2vec_inner.c'],
             include_dirs=[model_dir]),
+        Extension('gensim.models.visualword2vec_inner',
+            sources=['./gensim/models/visualword2vec_inner.c'],
+            include_dirs=[model_dir])
     ],
     cmdclass={'build_ext': custom_build_ext},
     packages=find_packages(),
